27213064727db183586188e164c46f4da7fdeb35
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-01 11:41:04 -0800

    Added some very basic styling to test out css

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -2,6 +2,6 @@ class TimesController < ApplicationController
   def main
   	@date = Time.now.strftime("%b %d, %Y")
   	@time = Time.now.strftime("%H:%M %p")
-  	puts @date
+  	# stylesheet_link_tag "style"
   end
 end
